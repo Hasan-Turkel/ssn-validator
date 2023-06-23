6920f11e6dbd53077bedb55a7492169e0555b353
hasan <mmhotmail>
2023-06-23 12:49:48 +0300

    second solution commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const input = document.querySelector("input");
 const article = document.querySelector("article");
 
 
-// ***************event*************
+// // ***************event*************
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
@@ -31,7 +31,7 @@ form.addEventListener("submit", (e)=>{
 
   else {
         invalid()
-        console.log(+arr[0]);
+        // console.log(+arr[0]);
         
     }
 
@@ -53,9 +53,79 @@ form.addEventListener("submit", (e)=>{
  article.className = "text-danger";}
     
  
- 
- 
- 
+// //  ************second solution ***********
 
+// // *******variables***** 
+
+// let condition1 = 0
+// let condition2 = 0
+// let condition3 = 0
+
+
+// // *****event****
+
+// form.addEventListener("submit", (e)=>{
+//   e.preventDefault();
+
+// arr =  input.value.split("-")
+ 
+//  if (!input.value.split("").includes("+") && !input.value.split("").includes(".") &&arr.length == 3 && arr[0].length == 3 && arr[1].length == 2 && arr[2].length == 4 ){
+
+
+
+  // for (let i = 1; i < 900; i++) {
+    
+  //   if (i==666) {
+  //     continue;
+//     }
+//     if (i==arr[0]){
+//       condition1=1;
+//       break;
+      
+//     }
+//   }
+//   for (let i = 1; i < 100; i++) {
+    
+//     if (i==arr[1]){
+//       condition2=1;
+//       break;
+//     }
+//   }
+//   for (let i = 1; i < 10000; i++) {
+//     if (i==arr[2]){
+//       condition3=1;
+//       break;
+//     }
+//   }
+// if(condition1 && condition2 && condition3){
+
+//   valid()
+// }
+// else{
+//   invalid()
+//   console.log(condition1);
+// }
+//  } 
+//  else{
+//   invalid()
+//  }
+
+//  form.reset()
+// }
+// )
+
+
+// // *************functions**********
+
+// const valid = () => {
+    
+//   article.innerText = `Entered SSN (${input.value}) is valid.`;
+//   article.className = "text-success";
+  
+// } 
+  
+// const invalid = () => {article.innerText = `Entered SSN (${input.value}) is invalid.
+// Please read the introduction carefully.`;
+// article.className = "text-danger";}
 
 
